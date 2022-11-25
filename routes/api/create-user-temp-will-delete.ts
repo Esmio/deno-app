c0851613fb6c6871621a45f7cb07cd466aa647a1
sailbirds <sailbirds@luminesence.jp>
2022-11-26 03:20:51 +0900

    init commit

---
+++ routes/api/create-user-temp-will-delete.ts
@@ -0,0 +1,27 @@
+import { HandlerContext } from '$fresh/server.ts';
+
+import User from '../../models/User.ts';
+
+class JsonResponse extends Response {
+  constructor(body?: Record<string, unknown> | BodyInit, init?: ResponseInit) {
+    if (!init) {
+      init = {};
+    }
+    if (!init.headers) {
+      init.headers = {};
+    }
+    init.headers = {
+      ...init.headers,
+      'content-type': 'application/json',
+    };
+    super(JSON.stringify(body), init);
+  }
+}
+
+export const handler = async (
+  _req: Request,
+  _ctx: HandlerContext
+): Promise<Response> => {
+  const user = await User.create({});
+  return new JsonResponse(user);
+};
---
+++ routes/index.tsx
@@ -0,0 +1,24 @@
+import { Head } from '$fresh/runtime.ts';
+import { Handlers, PageProps } from '$fresh/server.ts';
+import Counter from '../islands/Counter.tsx';
+import User from '../models/User.ts';
+
+export const handler: Handlers<User[]> = {
+  async GET(_, ctx) {
+    const users = await User.all();
+    return ctx.render(users);
+  },
+};
+
+export default function Home({ data }: PageProps<User[]>) {
+  return (
+    <>
+      <Head>
+        <title>Fresh App</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        There are {data.length} users!
+      </div>
+    </>
+  );
+}
